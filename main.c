703c677a2cfa3577dc062f18ec5a2682564a6fb4
David Kendall <david.kendall@northumbria.ac.uk>
2018-07-05 15:49:11 +0100

    Added functions for on and off

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
+#include <stdint.h>
 #include <stdbool.h>
-#include "MK64F12.h"
+
+/* Register address and mask definitions for the 
+ * System Clock Gating Control Register 5 
+ * See reference manual, chapter 12, pp 273ff
+ */
+#define SIM_SCGC5            (*((volatile uint32_t *)(0x40048038u)))
+#define SIM_SCGC5_PORTB_MASK (1u << 10)
+
+/* Register address and mask definitions for the Pin Control Register for
+ * pin 21 in PORT B
+ * See the reference manual, chapter 10, pp 243ff, and chapter 11, pp 273ff
+ */ 
+#define PORTB_PCR21          (*((volatile uint32_t *)(0x4004A054u)))
+#define PORTx_PCRn_MUX_MASK  (0x0700u)
+#define MUX_ALT1_MASK        (1u << 8)
+
+/* Register address and mask definitions for GPIO PORT B 
+ * Port Data Direction Register and Port Data Output Register
+ * See the reference manual, chapter 55, pp 1757ff
+ */ 
+#define GPIOB_PDDR           (*((volatile uint32_t *)(0x400FF054u)))
+#define GPIOB_PDOR           (*((volatile uint32_t *)(0x400FF040u)))
+#define PIN21_MASK           (1u << 21)
 
 void led_blue_init(void);
+void led_blue_on(void);
+void led_blue_off(void);
 void led_blue_toggle(void);
 void delay(int count);
 
@@ -10,20 +35,36 @@ int main(void)
     led_blue_init();
 
     while (true) {
-	led_blue_toggle();
-    	delay(2000);
+	led_blue_on();
+    	delay(1000);
+	led_blue_off();
+	delay(1000);
     }
     return 0;
 }
 
 void led_blue_init(void) {
-    SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; 
-    PORTB_PCR21 = PORT_PCR_MUX(1); 
-    GPIOB_PDDR |= (1 << 21);
+    /* Enable the clock to PORT B */
+    SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+
+    /* Select the GPIO function (Alternative 1) for pin 21 of PORT B */
+    PORTB_PCR21 &= ~PORTx_PCRn_MUX_MASK;
+    PORTB_PCR21 |= MUX_ALT1_MASK;
+
+    /* Set the data direction for pin 21 of PORT B to output */
+    GPIOB_PDDR |= PIN21_MASK;
+}
+
+void led_blue_on(void) {
+    GPIOB_PDOR &= ~PIN21_MASK; 
+}
+
+void led_blue_off(void) {
+    GPIOB_PDOR |= PIN21_MASK; 
 }
 
 void led_blue_toggle(void) {
-    GPIOB_PTOR |= (1 << 21); 
+    GPIOB_PDOR ^= PIN21_MASK; 
 }
 
 void delay(int count) {
